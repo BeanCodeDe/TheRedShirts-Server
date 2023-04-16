7326b1cddab038e548d93215004ef9841e517b01
Bean900 <niklas_nikisch@MDXN01006660.bku.db.de>
2023-04-16 15:16:03 +0200

    fix key generating file

--- authi-data/generateKeyFile.sh
+++ authi-data/generateKeyFile.sh
@@ -1,3 +1,2 @@
-mkdir ./authi-data/token
 ssh-keygen -t rsa -b 4096 -m PEM -f ./authi-data/token/jwtRS256.key
 openssl rsa -in ./authi-data/token/jwtRS256.key -pubout -outform PEM -out ./authi-data/token/jwtRS256.key.pub
